d312ad1eca36644a9571b6dd14583773254f48f4
whzsyx <3377107425@qq.com>
2021-09-24 08:19:16 +0800

    Update jd_jxmc_help.js

--- jd_jxmc_help.js
+++ jd_jxmc_help.js
@@ -137,7 +137,7 @@ function getShareCode(name) {
           console.log(`${JSON.stringify(err)}`);
           console.log(`${$.name} API请求失败，请检查网路重试`);
         } else {
-          console.log(`优先账号内部互助，有剩余助力次数再帮【zero205】助力`);
+          console.log(`优先账号内部互助`);
           $.shareCode = JSON.parse(data);
         }
       } catch (e) {
