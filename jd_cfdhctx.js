20293166e7cfac47a1f5cc78d17c35f3dd9de43c
whzsyx <3377107425@qq.com>
2021-10-29 00:53:22 +0800

    Update jd_cfdhctx.js

--- jd_cfdhctx.js
+++ jd_cfdhctx.js
@@ -6,7 +6,7 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message ='', index, nickName = '';
 let cfd_moon = process.env.CFD_MOON_NUM * 1;
-let   dwLvl=3;
+let   dwLvl=2;
 let   ddwVirHb=1000;
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
@@ -36,13 +36,13 @@ Date.prototype.Format = function (fmt) { //author: meizz
 !(async () => {
     if(cfd_moon === 5){
     //兑换5元红包
-        dwLvl=4;
+        dwLvl=3;
         ddwVirHb=500;
         console.log("*****你设置CFD_MOON_NUM兑换5元红包*****");
     }
     if(cfd_moon === 10){
     //兑换10元红包
-        dwLvl=3;
+        dwLvl=2;
         ddwVirHb=1000;
         console.log("*****你设置CFD_MOON_NUM兑换10元红包*****");
 
@@ -77,7 +77,7 @@ function taskPostUrl(){
 
 function taskUrl(cookie) {
             return {
-                "url": `https://m.jingxi.com/jxbfd/user/ExchangePearlHb?__t=1634920388835&strZone=jxbfd&dwLvl=${dwLvl}&dwIsRandHb=0&ddwVirHb=${ddwVirHb}&strPoolName=anhjZmQyX2V4Y2hhbmdlX2hjaGJfMjAyMTEwMjM=&dwExchangeType=0&_stk=__t%2CddwVirHb%2CdwExchangeType%2CdwIsRandHb%2CdwLvl%2CstrPoolName%2CstrZone&_ste=1&h5st=20211023003308851%3B4026339969767162%3B10032%3Btk01wbcd51c3b30n6ffjubBNLjmNobvZNoBLcI8rcGvRsgc7h8lm4SXDIKD%2B4JTc1wDHUytgd8CWw%2FcKtsT5Kc0zcS7V%3Bcce542e3cf1387a381022a9c3a1ac5f670247777578174d8994db7b89915bfcf&_=1634920388856&sceneval=2&g_login_type=1&callback=jsonpCBKH&g_ty=ls%0D%0A`,
+                "url": `https://m.jingxi.com/jxbfd/user/ExchangePearlHb?__t=1635439353425&strZone=jxbfd&dwLvl=${dwLvl}&dwIsRandHb=0&ddwVirHb=${ddwVirHb}&strPoolName=anhjZmQyX2V4Y2hhbmdlX2hjaGJfMjAyMTEwMjc=&dwExchangeType=0&_stk=__t%2CddwVirHb%2CdwExchangeType%2CdwIsRandHb%2CdwLvl%2CstrPoolName%2CstrZone&_ste=1&h5st=20211029004233426%3B4026339969767162%3B10032%3Btk01wbab91c6d30nUod%2FAleEO%2Bxsseqo%2FZrZoSFDyN7dlUuL5Hn%2B5aiRlz%2Bd%2BwvGj4YPJ4pMGx6fKs2ziYmj57dYULEj%3B081fe0afc28dcfdf51d53a3a4e9b8a7f3976285e39d4ab158c5cd693aefe456e&_=1635439353430&sceneval=2&g_login_type=1&callback=jsonpCBKH&g_ty=ls%0D%0A`,
                 "headers": {
                     "Host": "m.jingxi.com",
                     "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
