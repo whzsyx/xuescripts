8a4b65b93fbe2d70640ce7176adeeb4fda2ef8a2
whzsyx <3377107425@qq.com>
2021-11-04 15:02:00 +0800

    Update jd_zy_sjjc.js

--- jd_zy_sjjc.js
+++ jd_zy_sjjc.js
@@ -299,7 +299,7 @@ await quiz(quizId)
 async function readShareCodes(){
   return new Promise((resolve) => {
     let url = {
-   		url: `https://raw.githubusercontent.com/Ariszy/TGBOT/main/sjjc.js`,       
+   		url: ``,       
    	}
    $.get(url,async(error, response, data) =>{
     try{
