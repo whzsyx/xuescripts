3e1fddfa1318a8ff7060517f06de5f7dafbc1f32
whzsyx <3377107425@qq.com>
2021-11-19 14:17:05 +0800

    x

--- jd_getred.py
+++ jd_getred.py
@@ -3,6 +3,8 @@ const $ = new Env("统计东哥历史红包");
 统计东哥历史红包
 """
 
+
+
 import requests
 import sys
 import re
@@ -23,17 +25,6 @@ def getinfo(ck):
     isNext = True
     page = 1
     sum = 0
-    usedsum = 0
-    jxsum = 0
-    usedjx = 0
-    litesum = 0
-    usedlite = 0
-    healthsum = 0
-    usedhealth = 0
-    jdsum = 0
-    usedjd = 0
-    tysum = 0
-    usedty = 0
     count = 0
     while isNext:
         url = "https://wq.jd.com/user/info/QueryUserRedEnvelopesV2?type=2&orgFlag=JD_PinGou_New&page=%s&cashRedType=1&redBalanceFlag=0&channel=3&_=%s&sceneval=2&g_login_type=1&g_ty=ls" % (
@@ -51,33 +42,15 @@ def getinfo(ck):
             'cookie': ck
         }
         r = requests.get(url, headers=headers).json()
+        # print(r)
         if r['data']['unUseRedInfo']['redList'] == None:
-            print('最近六个月累计红包总数', count, '累计红包总额 %.2f' % sum, '已使用红包总额 %.2f' % usedsum)
-            print(
-                '\n其中：\n京东商城：总金额%.2f 已使用：%.2f\n京喜：总金额%.2f 已使用：%.2f\n极速版：总金额%.2f 已使用：%.2f\n京东健康：总金额%.2f 已使用：%.2f\n通用红包：总金额%.2f 已使用：%.2f\n' % (
-                jdsum, usedjd, jxsum, usedjx, litesum, usedlite, healthsum, usedhealth, tysum, usedty))
+            print('累计红包总数', count, '累计红包总额', sum)
             isNext = False
         else:
             page += 1
             count = r['data']['unUseRedInfo']['count']
             for i in r['data']['unUseRedInfo']['redList']:
                 sum += float(i['discount'])
-                usedsum += (float(i['discount']) - float(i['balance']))
-                if "京喜" in i['orgLimitStr']:
-                    jxsum += float(i['discount'])
-                    usedjx += (float(i['discount']) - float(i['balance']))
-                elif "极速" in i['orgLimitStr']:
-                    litesum += float(i['discount'])
-                    usedlite += (float(i['discount']) - float(i['balance']))
-                elif "健康" in i['orgLimitStr']:
-                    healthsum += float(i['discount'])
-                    usedhealth += (float(i['discount']) - float(i['balance']))
-                elif "京东商城" in i['orgLimitStr']:
-                    jdsum += float(i['discount'])
-                    usedjd += (float(i['discount']) - float(i['balance']))
-                else:
-                    tysum += float(i['discount'])
-                    usedty += (float(i['discount']) - float(i['balance']))
 
 
 if __name__ == '__main__':
